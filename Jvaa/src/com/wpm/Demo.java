6527cfc415e6dea0a0376c86fe8d4107fceb8596
painan <112233@qq.com>
2020-08-31 03:14:25 +0800

    tijiao

--- Jvaa/src/com/wpm/Demo.java
+++ Jvaa/src/com/wpm/Demo.java
@@ -4,11 +4,11 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("dev");
 		
 		
 		
-		
-		//dev
+		//dev  //dev
 	}
 
 }
