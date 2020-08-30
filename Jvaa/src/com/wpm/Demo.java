bfdf64761a77ef787f040c7e69eeb5bb105dcda1
painan <112233@qq.com>
2020-08-31 03:24:21 +0800

    tjiao

--- Jvaa/src/com/wpm/Demo.java
+++ Jvaa/src/com/wpm/Demo.java
@@ -5,8 +5,6 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("master");//master
-		System.out.println("master");//master
-		System.out.println("master");//master
 		
 		
 	}
