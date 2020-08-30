4d498e439ee5dc68f6da6db87fdd5f72ad03476f
painan <112233@qq.com>
2020-08-31 03:13:33 +0800

    tijiao

--- Jvaa/src/com/wpm/Demo.java
+++ Jvaa/src/com/wpm/Demo.java
@@ -3,12 +3,7 @@ package com.wpm;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("动次打次"); //master
 		
-		
-		
-		//dev
 	}
 
 }
